7e36350eb90f8f1b47dca30e2dc980ba8410278b
justinmmorgan21 <justinmorgan21@gmail.com>
2025-04-09 20:42:51 -0500

    added create action

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
 def get_db_connection():
@@ -17,7 +17,7 @@ def get_post(post_id):
     return post
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'ewdejh83idjok43d'
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -29,3 +29,20 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT into posts (title, content) VALUES (?,?)',
+                            (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template('create.html')
